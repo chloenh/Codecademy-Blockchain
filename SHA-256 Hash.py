47304566e9f7c7c547bf42bbd579a5c2330e2da3
chloenh <45791822+chloenh@users.noreply.github.com>
2019-02-19 22:50:19 +0100

    Update SHA-256 Hash.py

--- SHA-256 Hash.py
+++ SHA-256 Hash.py
@@ -9,3 +9,5 @@ text = 'My current company offers me internships in Shanghai and Warsaw'
 hash_result = sha256(text.encode())
 print(hash_result.hexdigest())
 
+#prints 16187538efed08c1a2f00df1e9560341a748a8aeeb46cb84d55c130908ecdd97
+
